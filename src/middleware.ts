a1f1748a56ad2d8f2a7bba15b8878fcb1cb95aab
Rémi Weng <ixartz@users.noreply.github.com>
2024-05-16 22:28:47 +0200

    feat: initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,61 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import {
+  type NextFetchEvent,
+  type NextRequest,
+  NextResponse,
+} from 'next/server';
+import createMiddleware from 'next-intl/middleware';
+
+import { AllLocales, AppConfig } from './utils/AppConfig';
+
+const intlMiddleware = createMiddleware({
+  locales: AllLocales,
+  localePrefix: AppConfig.localePrefix,
+  defaultLocale: AppConfig.defaultLocale,
+});
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/:locale/dashboard(.*)',
+  '/onboarding(.*)',
+  '/:locale/onboarding(.*)',
+]);
+
+export default function middleware(
+  request: NextRequest,
+  event: NextFetchEvent,
+) {
+  if (
+    request.nextUrl.pathname.includes('/sign-in') ||
+    request.nextUrl.pathname.includes('/sign-up') ||
+    isProtectedRoute(request)
+  ) {
+    return clerkMiddleware((auth, req) => {
+      const authObj = auth();
+
+      if (isProtectedRoute(req)) authObj.protect();
+
+      if (
+        authObj.userId &&
+        !authObj.orgId &&
+        req.nextUrl.pathname.includes('/dashboard') &&
+        !req.nextUrl.pathname.endsWith('/organization-selection')
+      ) {
+        const orgSelection = new URL(
+          '/onboarding/organization-selection',
+          req.url,
+        );
+
+        return NextResponse.redirect(orgSelection);
+      }
+
+      return intlMiddleware(req);
+    })(request, event);
+  }
+
+  return intlMiddleware(request);
+}
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
