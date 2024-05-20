6d7a45bf4acc5a44568c39462f6c1561cbe08ebc
Rémi Weng <ixartz@users.noreply.github.com>
2024-05-20 02:28:34 +0200

    feat: only admin of an organization can manage billing

--- src/app/[locale]/(auth)/dashboard/billing/page.tsx
+++ src/app/[locale]/(auth)/dashboard/billing/page.tsx
@@ -1,18 +1,21 @@
+import { Protect } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs/server';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { getTranslations } from 'next-intl/server';
 
 import { buttonVariants } from '@/components/ui/button';
+import { ProtectFallback } from '@/features/auth/ProtectFallback';
 import { BillingOptions } from '@/features/billing/BillingOptions';
 import { CurrentPlanDetails } from '@/features/billing/CurrentPlanDetails';
 import { DashboardSection } from '@/features/dashboard/DashboardSection';
 import { TitleBar } from '@/features/dashboard/TitleBar';
 import { determineSubscriptionPlan } from '@/services/BillingService';
 import { getStripeSubscription } from '@/services/OrganizationService';
+import { ORG_ROLE } from '@/types/Auth';
 
 const BillingPage = async (props: { params: { locale: string } }) => {
-  const { orgId } = auth();
+  const { orgId, has } = auth();
 
   if (!orgId) {
     redirect('/onboarding/organization-selection');
@@ -40,17 +43,35 @@ const BillingPage = async (props: { params: { locale: string } }) => {
 
         {stripeDetails?.stripeCustomerId && (
           <div className="mt-5">
-            <Link
-              className={buttonVariants({ size: 'lg' })}
-              href="/dashboard/billing/portal"
+            <Protect
+              role={ORG_ROLE.ADMIN}
+              fallback={
+                <ProtectFallback
+                  trigger={
+                    <div
+                      className={buttonVariants({
+                        size: 'lg',
+                        variant: 'secondary',
+                      })}
+                    >
+                      {t('manage_subscription_button')}
+                    </div>
+                  }
+                />
+              }
             >
-              {t('manage_subscription_button')}
-            </Link>
+              <Link
+                className={buttonVariants({ size: 'lg' })}
+                href="/dashboard/billing/portal"
+              >
+                {t('manage_subscription_button')}
+              </Link>
+            </Protect>
           </div>
         )}
       </DashboardSection>
 
-      {!planDetails.isPaid && (
+      {has({ role: ORG_ROLE.ADMIN }) && !planDetails.isPaid && (
         <div className="mt-8">
           <BillingOptions />
         </div>
