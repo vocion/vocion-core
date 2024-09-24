3b118fb284b38a3ec4056f0881dc14be79ac9439
Rémi Weng <ixartz@users.noreply.github.com>
2024-09-24 23:41:52 +0200

    test: add integration tests with incorrect parameters for Todo

--- tests/integration/Todo.spec.ts
+++ tests/integration/Todo.spec.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { expect, test } from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
 import { AUTH_FILE } from '../TestUtils';
 
@@ -7,6 +7,36 @@ test.use({ storageState: AUTH_FILE });
 
 test.describe('Todo', () => {
   test.describe('Basic CRUD operations', () => {
+    test('should return an error when creating a todo with missing data', async ({ request }) => {
+      const createResponse = await request.post('/api/todo', {
+        data: {},
+      });
+
+      expect(createResponse.status()).toBe(422);
+    });
+
+    test('should return an error when editing a non-existing todo', async ({ request }) => {
+      const editResponse = await request.put('/api/todo', {
+        data: {
+          id: '123',
+          title: faker.word.words(3),
+          message: faker.word.words(10),
+        },
+      });
+
+      expect(editResponse.status()).toBe(404);
+    });
+
+    test('should return an error when deleting a non-existing todo', async ({ request }) => {
+      const deleteResponse = await request.delete('/api/todo', {
+        data: {
+          id: '123',
+        },
+      });
+
+      expect(deleteResponse.status()).toBe(404);
+    });
+
     test('should create a new todo and edit it without error', async ({
       request,
     }) => {
