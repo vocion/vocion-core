84def626b86a96bb55347f6a698e1f5e0bd924b7
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-09 03:26:33 +0200

    refactor: remove unnecessary toLocaleLowerCase function

--- src/app/[locale]/(auth)/dashboard/billing/checkout/[planId]/route.ts
+++ src/app/[locale]/(auth)/dashboard/billing/checkout/[planId]/route.ts
@@ -29,7 +29,7 @@ export async function GET(
   }
 
   const plan = PricingPlanList.find(
-    elt => elt.id.toLocaleLowerCase() === context.params.planId,
+    elt => elt.id === context.params.planId,
   );
 
   if (!plan) {
