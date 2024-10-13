899e12b427fff992c4d9c71292da0954d618f14e
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-13 22:02:22 +0200

    fix: implement dark mode for Clerk component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -67,7 +67,6 @@ export default function RootLayout(props: {
             {props.children}
 
             <DemoBadge />
-
           </NextIntlClientProvider>
         </ThemeProvider>
       </body>
