92197f67e74530395a8ffe00cead14af48fbbb44
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-15 17:16:58 +0200

    fix: add sticky banner in landing page

---
+++ src/templates/DemoBanner.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+import { StickyBanner } from '@/features/landing/StickyBanner';
+
+export const DemoBanner = () => (
+  <StickyBanner>
+    Live Demo of Next.js Boilerplate SaaS -
+    {' '}
+    <Link href="/sign-up">Explore the User Dashboard</Link>
+  </StickyBanner>
+);
