54b5afba425ed30e7824338ea190273e1c5a33fa
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-21 15:14:45 +0200

    refactor: create a new component for AppSidebar header

--- src/app/[locale]/(auth)/dashboard/layout.tsx
+++ src/app/[locale]/(auth)/dashboard/layout.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from '@clerk/nextjs';
 import { getTranslations } from 'next-intl/server';
 
-import { DarkModeToggle } from '@/components/DarkModeToggle';
-import { LocaleSwitcher } from '@/components/LocaleSwitcher';
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/features/dashboard/AppSidebar';
+import { AppSidebarHeader } from '@/features/dashboard/AppSidebarHeader';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
@@ -23,33 +21,7 @@ export default function DashboardLayout(props: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-          </div>
-
-          <ul className="flex items-center gap-x-1.5 [&_li:not(:last-child):hover]:opacity-100 [&_li:not(:last-child)]:opacity-60">
-            <li>
-              <DarkModeToggle />
-            </li>
-
-            <li>
-              <LocaleSwitcher />
-            </li>
-
-            <li>
-              <UserButton
-                userProfileMode="navigation"
-                userProfileUrl="/dashboard/user-profile"
-                appearance={{
-                  elements: {
-                    rootBox: 'px-2 py-1.5',
-                  },
-                }}
-              />
-            </li>
-          </ul>
-        </header>
+        <AppSidebarHeader />
 
         <div className="flex-1 px-6 pt-4">
           {props.children}
---
+++ src/features/dashboard/AppSidebarHeader.tsx
@@ -0,0 +1,35 @@
+import { UserButton } from '@clerk/nextjs';
+
+import { DarkModeToggle } from '@/components/DarkModeToggle';
+import { LocaleSwitcher } from '@/components/LocaleSwitcher';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+
+export const AppSidebarHeader = () => (
+  <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-2">
+    <div className="flex items-center gap-2 px-4">
+      <SidebarTrigger className="-ml-1" />
+    </div>
+
+    <ul className="flex items-center gap-x-1.5 [&_li:not(:last-child):hover]:opacity-100 [&_li:not(:last-child)]:opacity-60">
+      <li>
+        <DarkModeToggle />
+      </li>
+
+      <li>
+        <LocaleSwitcher />
+      </li>
+
+      <li>
+        <UserButton
+          userProfileMode="navigation"
+          userProfileUrl="/dashboard/user-profile"
+          appearance={{
+            elements: {
+              rootBox: 'px-2 py-1.5',
+            },
+          }}
+        />
+      </li>
+    </ul>
+  </header>
+);
