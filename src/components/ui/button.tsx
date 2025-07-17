3c6a539c05de19c42a6869100ad3f262d62990ed
Rémi Weng <ixartz@users.noreply.github.com>
2025-07-17 15:58:19 +0200

    fix: update Shadcn UI button to the latest version

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,23 +1,27 @@
 import type { VariantProps } from 'class-variance-authority';
 import { Slot } from '@radix-ui/react-slot';
-import * as React from 'react';
 import { cn } from '@/utils/Helpers';
 import { buttonVariants } from './buttonVariants';
 
-export type ButtonProps = {
-  asChild?: boolean;
-} & React.ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants>;
-
-const Button = ({ ref, className, variant, size, asChild = false, ...props }: ButtonProps & { ref?: React.RefObject<HTMLButtonElement | null> }) => {
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'>
+  & VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
   const Comp = asChild ? Slot : 'button';
+
   return (
     <Comp
+      data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
-      ref={ref}
       {...props}
     />
   );
-};
-Button.displayName = 'Button';
+}
 
 export { Button };
