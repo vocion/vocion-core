692856356f66d3109c980378b4678306f79b843d
Rémi Weng <ixartz@users.noreply.github.com>
2025-07-24 16:13:35 +0200

    feat: migrate to orpc for editing todo

--- src/routers/todo.ts
+++ src/routers/todo.ts
@@ -2,8 +2,9 @@ import { auth } from '@clerk/nextjs/server';
 import { ORPCError, os } from '@orpc/server';
 import { headers } from 'next/headers';
 import { logger } from '@/libs/Logger';
-import { createTodo } from '@/services/TodoService';
-import { TodoValidation } from '@/validations/TodoValidation';
+import { createTodo, updateTodo } from '@/services/TodoService';
+import { ORG_ROLE } from '@/types/Auth';
+import { EditTodoValidation, TodoValidation } from '@/validations/TodoValidation';
 
 export const ping = os
   .use(async ({ next }) => next({
@@ -25,7 +26,7 @@ export const create = os
     const { userId, orgId } = await auth();
 
     if (!userId || !orgId) {
-      throw new ORPCError('Unauthorized');
+      throw new ORPCError('Unauthorized', { status: 401 });
     }
 
     const todo = await createTodo(input, orgId);
@@ -35,4 +36,28 @@ export const create = os
     return {
       id: todo[0]?.id,
     };
+  });
+
+export const edit = os
+  .input(EditTodoValidation)
+  .handler(async ({ input }) => {
+    const { userId, orgId, has } = await auth();
+
+    if (!userId || !orgId) {
+      throw new ORPCError('Unauthorized', { status: 401 });
+    }
+
+    if (!has({ role: ORG_ROLE.ADMIN })) {
+      throw new ORPCError('Forbidden', { status: 403 });
+    }
+
+    const result = await updateTodo(input, orgId);
+
+    if (result.length === 0) {
+      throw new ORPCError('Todo not found', { status: 404 });
+    }
+
+    logger.info('A todo has been updated');
+
+    return {};
   });
