db59b667e7bec253d24859fe49d1cf1d6ab5bf3b
Rémi Weng <ixartz@users.noreply.github.com>
2025-07-25 21:58:46 +0200

    fix: update integration tests after migrating to ORPC

--- tests/integration/Todo.spec.ts
+++ tests/integration/Todo.spec.ts
@@ -9,19 +9,23 @@ test.describe('Todo', () => {
 
   test.describe('Basic CRUD operations', () => {
     test('should return an error when creating a todo with missing data', async ({ page }) => {
-      const createResponse = await page.request.post('/api/todos', {
-        data: {},
+      const createResponse = await page.request.post('/rpc/todo/create', {
+        data: {
+          json: {},
+        },
       });
 
-      expect(createResponse.status()).toBe(422);
+      expect(createResponse.status()).toBe(400);
     });
 
     test('should return an error when editing a non-existing todo', async ({ page }) => {
-      const editResponse = await page.request.put('/api/todos', {
+      const editResponse = await page.request.post('/rpc/todo/edit', {
         data: {
-          id: '123',
-          title: faker.word.words(3),
-          message: faker.word.words(10),
+          json: {
+            id: '123',
+            title: faker.word.words(3),
+            message: faker.word.words(10),
+          },
         },
       });
 
@@ -29,9 +33,11 @@ test.describe('Todo', () => {
     });
 
     test('should return an error when deleting a non-existing todo', async ({ page }) => {
-      const deleteResponse = await page.request.delete('/api/todos', {
+      const deleteResponse = await page.request.post('/rpc/todo/remove', {
         data: {
-          id: '123',
+          json: {
+            id: '123',
+          },
         },
       });
 
@@ -41,22 +47,26 @@ test.describe('Todo', () => {
     test('should create a new todo and edit it without error', async ({
       page,
     }) => {
-      const createResponse = await page.request.post('/api/todos', {
+      const createResponse = await page.request.post('/rpc/todo/create', {
         data: {
-          title: faker.word.words(3),
-          message: faker.word.words(10),
+          json: {
+            title: faker.word.words(3),
+            message: faker.word.words(10),
+          },
         },
       });
-      const createJson = await createResponse.json();
+      const createJson = (await createResponse.json()).json;
 
       expect(createResponse.status()).toBe(200);
       expect(createJson.id).toBeDefined();
 
-      const editResponse = await page.request.put('/api/todos', {
+      const editResponse = await page.request.post('/rpc/todo/edit', {
         data: {
-          id: createJson.id,
-          title: faker.word.words(3),
-          message: faker.word.words(10),
+          json: {
+            id: createJson.id,
+            title: faker.word.words(3),
+            message: faker.word.words(10),
+          },
         },
       });
 
@@ -66,20 +76,24 @@ test.describe('Todo', () => {
     test('should create a new todo and delete it without error', async ({
       page,
     }) => {
-      const createResponse = await page.request.post('/api/todos', {
+      const createResponse = await page.request.post('/rpc/todo/create', {
         data: {
-          title: faker.word.words(3),
-          message: faker.word.words(10),
+          json: {
+            title: faker.word.words(3),
+            message: faker.word.words(10),
+          },
         },
       });
-      const createJson = await createResponse.json();
+      const createJson = (await createResponse.json()).json;
 
       expect(createResponse.status()).toBe(200);
       expect(createJson.id).toBeDefined();
 
-      const deleteResponse = await page.request.delete('/api/todos', {
+      const deleteResponse = await page.request.post('/rpc/todo/remove', {
         data: {
-          id: createJson.id,
+          json: {
+            id: createJson.id,
+          },
         },
       });
 
