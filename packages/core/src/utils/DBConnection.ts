8924e31968ae4ac4a9626a0b19084e3128f300e1
Chris Fitkin <cfitkin@gmail.com>
2026-06-19 15:25:27 -0700

    chore: remove upstream Vocion references

--- packages/core/src/utils/DBConnection.ts
+++ packages/core/src/utils/DBConnection.ts
@@ -3,8 +3,6 @@ import { Pool } from 'pg';
 import { Env } from '@/libs/Env';
 import * as schema from '@/models/Schema';
 
-// Need a database for production? Check out https://www.prisma.io/?via=vocioncore
-// Tested and compatible with Vocion
 export const createDbConnection = () => {
   const pool = new Pool({
     connectionString: Env.DATABASE_URL,
